1f6e0b5140bb0167d6c21fcc35ab5379d33f2905
suwan98 <imissyou5466@gmail.com>
2023-03-21 18:57:03 +0900

    2023-03-21

--- app.js
+++ app.js
@@ -18,21 +18,34 @@ const countMaker = function () {
   const reMaining = (targetDate - nowDate) / 1000;
   if (reMaining <= 0) {
     Mcontainer.innerHTML = "<h3>타이머가 종료되었습니다</h3>";
+    Mcontainer.style.display = "flex";
+    return;
   } else if (isNaN(reMaining)) {
+    container.style.display = "none";
     Mcontainer.innerHTML = "<h3>유효한 시간대가 아닙니다</h3>";
+    Mcontainer.style.display = "flex";
+    return;
   }
-  const reMainingDate = Math.floor(reMaining / 3600 / 24);
-  const reMainingHours = Math.floor(reMaining / 3600) % 24;
-  const reMainingMin = Math.floor(reMaining / 60) % 60;
-  const reMainingSec = Math.floor(reMaining) % 60;
 
-  const days = document.querySelector("#days");
-  const hours = document.querySelector("#hours");
-  const min = document.querySelector("#min");
-  const sec = document.querySelector("#sec");
+  const reMainingObj = {
+    reMainingDate: Math.floor(reMaining / 3600 / 24),
+    reMainingHours: Math.floor(reMaining / 3600) % 24,
+    reMainingMin: Math.floor(reMaining / 60) % 60,
+    reMainingSec: Math.floor(reMaining) % 60,
+  };
 
-  days.textContent = reMainingDate;
-  hours.textContent = reMainingHours;
-  min.textContent = reMainingMin;
-  sec.textContent = reMainingSec;
+  const documentArr = ["days", "hours", "min", "sec"];
+  const timeKeys = Object.keys(reMainingObj);
+
+  let i = 0;
+  for (let tag of documentArr) {
+    document.getElementById(tag).textContent = reMainingObj[timeKeys[i]];
+    i++;
+  }
+};
+
+const starter = () => {
+  container.style.display = "flex";
+  Mcontainer.style.display = "none";
+  countMaker();
 };
